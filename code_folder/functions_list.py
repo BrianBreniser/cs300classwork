4452bc7d254c8db5b91ae8d4a2fc290954518bd5
Brian Breniser <brianbreniser@gmail.com>
2014-11-26 15:04:16 -0800

    members().addmember() works and tested

--- code_folder/functions_list.py
+++ code_folder/functions_list.py
@@ -1,12 +1,52 @@
 #! /usr/bin/env python
 
-# from json import ...
+from json import load, dump
+from os import remove, path
 
 
-class customers():
+class v(object):
+
+    """standard variables used in this program."""
+
+    memberlist = "memberlist.json"
+
+
+class customers(object):
 
     """a list of customers."""
 
     def addcustomer(self):
         """my docstring."""
         return "hello world"
+
+
+class member(object):
+
+    """a member of the choc-an system."""
+
+    def addmember(self, name=None, number=None, address=None, city=None, state=None, zip=None):
+        """add a member to the members file, creates the file if it does not exist."""
+        listitem = {"name": name,
+                    "number": number,
+                    "address": address,
+                    "city": city,
+                    "state": state,
+                    "zip": zip}
+
+        if not path.exists(v.memberlist):
+            fp = open(v.memberlist, 'w+b')
+            fp.close()
+        fp = open(v.memberlist, 'rb')
+
+        try:
+            memberlist = load(fp)
+        except ValueError:
+            memberlist = list()
+        except:
+            print "unhandled exception occured"
+
+        fp.close()
+        fp = open(v.memberlist, 'w+b')
+        memberlist.append(listitem)
+        dump(memberlist, fp)
+        fp.close()
--- code_folder/tests.py
+++ code_folder/tests.py
@@ -1,6 +1,51 @@
 #! /usr/bin/env python
 
 import functions_list as f
+from os import path, remove
+from json import load
+
+# testing tests please move on --------------------------------------------------
 
 x = f.customers()
 assert x.addcustomer() == "hello world", "customer != 'hello world'"
+
+# done testing tests ------------------------------------------------------------
+
+# tests of member() class -------------------------------------------------------
+
+# add member test---------------------------------------------
+
+# clear memberlist file
+if path.exists(f.v.memberlist):
+    remove(f.v.memberlist)
+
+# test to see if adding from empty list works
+f.member().addmember(name="joe", number="0123456789", address="101 n whatever street", city="portland", state="OR", zip="912345")
+fp = open(f.v.memberlist, 'rb')
+try:
+    mylist = load(fp)
+except ValueError:
+    print "load failed"
+
+assert mylist[0]["name"] == "joe"
+assert mylist[0]["number"] == "0123456789"
+assert mylist[0]["address"] == "101 n whatever street"
+assert mylist[0]["city"] == "portland"
+assert mylist[0]["state"] == "OR"
+assert mylist[0]["zip"] == "912345"
+
+# test to see if addine a second one works
+
+f.member().addmember(name="jeff", number="987654321", address="you better get this right", city="greshem", state="why not", zip="74635273849")
+fp = open(f.v.memberlist, 'rb')
+try:
+    mylist = load(fp)
+except ValueError:
+    print "load failed"
+
+assert mylist[1]["name"] == "jeff"
+assert mylist[1]["number"] == "987654321"
+assert mylist[1]["address"] == "you better get this right"
+assert mylist[1]["city"] == "greshem"
+assert mylist[1]["state"] == "why not"
+assert mylist[1]["zip"] == "74635273849"
