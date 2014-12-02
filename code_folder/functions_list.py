e74615297e584d9a73fcddc2f4193ae5880cec67
Brian Breniser <brianbreniser@gmail.com>
2014-12-02 12:10:57 -0800

    removed .json tracking, updated functions, still testing, backing up

--- code_folder/functions_list.py
+++ code_folder/functions_list.py
@@ -14,6 +14,7 @@ class v(object):
     memberalreadyfound = "memberalreadyfound"
     missingdata = "missingdata"
     toolongdata = "toolongdata"
+    nonefound = "nonefound"
 
 
 class member(object):
@@ -23,12 +24,15 @@ class member(object):
     def addmember(self, name=None, number=None, address=None, city=None, state=None, zipcode=None):
         """add a member to the members file, creates the file if it does not exist."""
 
+        # Check for non-entered data
         if name is None or number is None or address is None or city is None or state is None or zipcode is None:
             return v.missingdata
 
+        # Check for length attributes
         if len(name) > 25 or len(number) > 9 or len(address) > 25 or len(city) > 14 or len(state) > 2 or len(zipcode) > 5:
             return v.toolongdata
 
+        # Prep our list item to be added to file
         listitem = {"name": name,
                     "number": number,
                     "address": address,
@@ -36,11 +40,15 @@ class member(object):
                     "state": state,
                     "zipcode": zipcode}
 
+        # Check to see if member file exists, if not, create it and close it
         if not path.exists(v.memberlist):
             fp = open(v.memberlist, 'w+b')
             fp.close()
+
+        # Open file (which will exist because the last secion made it exists no matter what)
         fp = open(v.memberlist, 'rb')
 
+        # try to load the file from JSON format, if it fails, initiate an empty list
         try:
             memberlist = load(fp)
         except ValueError:
@@ -49,17 +57,22 @@ class member(object):
             print "unhandled exception occured"
             return 0
 
-        # Got to check t osee if we already have this member (number) in our system
+        # Done with reading the file
+        fp.close()
+
+        # Got to check to see if we already have this member (number) in our system
         for m in memberlist:
             if m["number"] == number:
                 return v.memberalreadyfound
 
-        fp.close()
-        fp = open(v.memberlist, 'w+b')
+        # If it all checks out, we can add our new listitem
         memberlist.append(listitem)
+
+        # open file (while destroying current data) for writing, write the new data
+        fp = open(v.memberlist, 'w+b')
         dump(memberlist, fp)
         fp.close()
-        return True
+        return True  # return a success if we get to this point
 
     def display(self):
         """display all members of the choc-an members list."""
@@ -92,3 +105,95 @@ class member(object):
         fp = open(v.memberlist, 'w+b')
         fp.close()
         return True
+
+    def deletemember(self, number=None):
+        """Delete only a single member by member number."""
+        # If no data was passed in, don't do anything
+        if number is None:
+            return v.missingdata
+
+        # Check to see if member file exists, if not, create it and close it
+        if not path.exists(v.memberlist):
+            fp = open(v.memberlist, 'w+b')
+            fp.close()
+
+        # Open file (which will exist because the last secion made it exists no matter what)
+        fp = open(v.memberlist, 'rb')
+
+        # try to load the file from JSON format, if it fails, initiate an empty list
+        try:
+            memberlist = load(fp)
+        except ValueError:
+            memberlist = list()
+        except:
+            print "unhandled exception occured"
+            return 0
+
+        # Done with reading the file
+        fp.close()
+
+        # Check to see if member number is in the list
+        for m in memberlist:
+            if m["number"] == number:
+                # we found it! Remove it from the list!
+                memberlist.remove(m)
+
+                # open file (while destroying current data) for writing, write the new data
+                fp = open(v.memberlist, 'w+b')
+                dump(memberlist, fp)
+                fp.close()
+                return True  # return a success if we get to this point
+
+        # If we made it here, we didn't find the item
+        return v.nonefound
+
+    def modifymember(self, name=None, number=None, address=None, city=None, state=None, zipcode=None):
+        """modify a member by searching for member number and overwriting other fields entered."""
+        # If we didn't give a member number, we don't know what to change
+        if number is None:
+            return v.missingdata
+
+        # Check to see if member file exists, if not, create it and close it
+        if not path.exists(v.memberlist):
+            fp = open(v.memberlist, 'w+b')
+            fp.close()
+
+        # Open file (which will exist because the last secion made it exists no matter what)
+        fp = open(v.memberlist, 'rb')
+
+        # try to load the file from JSON format, if it fails, initiate an empty list
+        try:
+            memberlist = load(fp)
+        except ValueError:
+            memberlist = list()
+        except:
+            print "unhandled exception occured"
+            return 0
+
+        # Done with reading the file
+        fp.close()
+
+        # Check to see if member number is in the list
+        for m in memberlist:
+            if m["number"] == number:
+                # we found it! Start changin data
+                # For each item, if we passed in data, update the list
+                if name is not None:
+                    m["name"] = name
+                if address is not None:
+                    m["address"] = address
+                if city is not None:
+                    m["city"] = city
+                if state is not None:
+                    m["state"] = state
+                if zipcode is not None:
+                    m["zipcode"] = zipcode
+
+                # open file (while destroying current data) for writing, write the new data
+                fp = open(v.memberlist, 'w+b')
+                dump(memberlist, fp)
+                fp.close()
+                return True  # return a success if we get to this point
+
+        # If we made it here, we didn't find the item
+        return v.nonefound
--- code_folder/tests.py
+++ code_folder/tests.py
@@ -68,13 +68,100 @@ def displaymembertest():
     # f.member().addmember(name="jeff", number="9876543", address="new address", city="greshem", state="WA", zipcode="543")
     # assert f.member().display() == True
 
+    # clean up after test
+    if path.exists(f.v.memberlist):
+        remove(f.v.memberlist)
+
 
 def deletealltest():
-    """ test delete all funciton. """
+    """ test delete all member function. """
     f.member().addmember(name="joe", number="12345678", address="101 n whatever street", city="portland", state="OR", zipcode="12345")
     f.member().addmember(name="jeff", number="9876543", address="new address", city="greshem", state="WA", zipcode="543")
     assert f.member().deleteall() is True
     assert f.member().display() is f.v.displayerror
+
+    # clean up after test
+    if path.exists(f.v.memberlist):
+        remove(f.v.memberlist)
+
+
+def deletemembertest():
+    """ test delete one member function. """
+    f.member().addmember(name="joe", number="12345678", address="101 n whatever street", city="portland", state="OR", zipcode="12345")
+    f.member().addmember(name="jeff", number="9876543", address="new address", city="greshem", state="WA", zipcode="543")
+    f.member().addmember(name="jeff", number="6496743", address="my address0", city="greshem", state="WA", zipcode="543")
+    assert f.member().deletemember("9876543") is True
+    assert f.member().deletemember("1029384") is f.v.nonefound
+    assert f.member().deletemember(None) is f.v.missingdata
+
+    fp = open(f.v.memberlist, 'rb')
+    try:
+        mylist = load(fp)
+    except ValueError:
+        print "load failed"
+
+    assert mylist[0]["name"] == "joe"
+    assert mylist[0]["number"] == "12345678"
+    assert mylist[0]["address"] == "101 n whatever street"
+    assert mylist[0]["city"] == "portland"
+    assert mylist[0]["state"] == "OR"
+    assert mylist[0]["zipcode"] == "12345"
+
+    assert mylist[1]["name"] == "jeff"
+    assert mylist[1]["number"] == "6496743"
+    assert mylist[1]["address"] == "my address0"
+    assert mylist[1]["city"] == "greshem"
+    assert mylist[1]["state"] == "WA"
+    assert mylist[1]["zipcode"] == "543"
+
+    # clean up after test
+    if path.exists(f.v.memberlist):
+        remove(f.v.memberlist)
+
+
+def modifymembertest():
+    """ Test modify member function. """
+    f.member().addmember(name="joe", number="12345678", address="101 n whatever street", city="portland", state="OR", zipcode="12345")
+    f.member().addmember(name="jeff", number="6496743", address="my address0", city="greshem", state="WA", zipcode="543")
+    f.member().addmember(name="mark", number="12345610", address="101 n whatever street", city="portland", state="OR", zipcode="12345")
+
+    assert f.member().modifymember(number="12345678") is True
+    assert f.member().modifymember(number="6496743", name="foobar") is True
+    assert f.member().modifymember(number="12345610", name="mark0", address="101 n whatever street0", city="portland0", state="OR", zipcode="12340") is True
+
+    assert f.member().modifymember(number="1234569", name="mark", state="WA") is f.v.nonefound
+    assert f.member().modifymember(number=None, name="sarah", state="NY") is f.v.missingdata
+
+    fp = open(f.v.memberlist, 'rb')
+    try:
+        mylist = load(fp)
+    except ValueError:
+        print "load failed"
+
+    assert mylist[0]["name"] == "joe"
+    assert mylist[0]["number"] == "12345678"
+    assert mylist[0]["address"] == "101 n whatever street"
+    assert mylist[0]["city"] == "portland"
+    assert mylist[0]["state"] == "OR"
+    assert mylist[0]["zipcode"] == "12345"
+
+    assert mylist[1]["name"] == "foobar"
+    assert mylist[1]["number"] == "6496743"
+    assert mylist[1]["address"] == "my address0"
+    assert mylist[1]["city"] == "greshem"
+    assert mylist[1]["state"] == "WA"
+    assert mylist[1]["zipcode"] == "543"
+
+    assert mylist[0]["name"] == "mark0"
+    assert mylist[0]["number"] == "12345610"
+    assert mylist[0]["address"] == "101 n whatever street0"
+    assert mylist[0]["city"] == "portland0"
+    assert mylist[0]["state"] == "OR"
+    assert mylist[0]["zipcode"] == "12340"
+
+    # clean up after test
+    if path.exists(f.v.memberlist):
+        remove(f.v.memberlist)
 
 # run main----------------------------------------------------
 
@@ -84,6 +171,8 @@ def main():
     addmembertest()
     displaymembertest()
     deletealltest()
+    deletemembertest()
+    modifymembertest()
 
 
 # done with main
